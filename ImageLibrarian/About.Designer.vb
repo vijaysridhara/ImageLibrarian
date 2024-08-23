0d5766e43fdb9dd5f06ff80715bbad520eed042c
vijaysridhara <vitallogic@gmail.com>
2024-08-23 18:40:04 +0530

    About box version not reflecting 0.4.8

--- ImageLibrarian/About.Designer.vb
+++ ImageLibrarian/About.Designer.vb
@@ -22,94 +22,93 @@ Partial Class About
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.lblProductname = New System.Windows.Forms.Label()
-        Me.lblVersion = New System.Windows.Forms.Label()
-        Me.lblCopyright = New System.Windows.Forms.Label()
-        Me.lblDescription = New System.Windows.Forms.Label()
-        Me.butCancel = New System.Windows.Forms.Button()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.VijaySridhara.Applications.My.Resources.Resources.piclib_128
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 23)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(127, 126)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
-        '
-        'lblProductname
-        '
-        Me.lblProductname.AutoSize = True
-        Me.lblProductname.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lblProductname.Location = New System.Drawing.Point(143, 32)
-        Me.lblProductname.Name = "lblProductname"
-        Me.lblProductname.Size = New System.Drawing.Size(70, 25)
-        Me.lblProductname.TabIndex = 1
-        Me.lblProductname.Text = "Label1"
-        '
-        'lblVersion
-        '
-        Me.lblVersion.AutoSize = True
-        Me.lblVersion.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lblVersion.Location = New System.Drawing.Point(143, 57)
-        Me.lblVersion.Name = "lblVersion"
-        Me.lblVersion.Size = New System.Drawing.Size(67, 25)
-        Me.lblVersion.TabIndex = 1
-        Me.lblVersion.Text = "Label1"
-        '
-        'lblCopyright
-        '
-        Me.lblCopyright.AutoSize = True
-        Me.lblCopyright.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lblCopyright.Location = New System.Drawing.Point(145, 93)
-        Me.lblCopyright.Name = "lblCopyright"
-        Me.lblCopyright.Size = New System.Drawing.Size(56, 21)
-        Me.lblCopyright.TabIndex = 1
-        Me.lblCopyright.Text = "Label1"
-        '
-        'lblDescription
-        '
-        Me.lblDescription.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.lblDescription.Location = New System.Drawing.Point(12, 164)
-        Me.lblDescription.Name = "lblDescription"
-        Me.lblDescription.Size = New System.Drawing.Size(395, 189)
-        Me.lblDescription.TabIndex = 1
-        Me.lblDescription.Text = "Label1"
-        '
-        'butCancel
-        '
-        Me.butCancel.Location = New System.Drawing.Point(330, 365)
-        Me.butCancel.Name = "butCancel"
-        Me.butCancel.Size = New System.Drawing.Size(77, 35)
-        Me.butCancel.TabIndex = 20
-        Me.butCancel.Text = "E&xit"
-        Me.butCancel.UseVisualStyleBackColor = True
-        '
-        'About
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.CancelButton = Me.butCancel
-        Me.ClientSize = New System.Drawing.Size(420, 404)
-        Me.Controls.Add(Me.butCancel)
-        Me.Controls.Add(Me.lblDescription)
-        Me.Controls.Add(Me.lblCopyright)
-        Me.Controls.Add(Me.lblVersion)
-        Me.Controls.Add(Me.lblProductname)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-        Me.KeyPreview = True
-        Me.Name = "About"
-        Me.ShowIcon = False
-        Me.ShowInTaskbar = False
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "About"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+        PictureBox1 = New PictureBox()
+        lblProductname = New Label()
+        lblVersion = New Label()
+        lblCopyright = New Label()
+        lblDescription = New Label()
+        butCancel = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.piclib_128
+        PictureBox1.Location = New Point(12, 23)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(127, 126)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' lblProductname
+        ' 
+        lblProductname.AutoSize = True
+        lblProductname.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        lblProductname.Location = New Point(143, 32)
+        lblProductname.Name = "lblProductname"
+        lblProductname.Size = New Size(70, 25)
+        lblProductname.TabIndex = 1
+        lblProductname.Text = "Label1"
+        ' 
+        ' lblVersion
+        ' 
+        lblVersion.AutoSize = True
+        lblVersion.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        lblVersion.Location = New Point(143, 57)
+        lblVersion.Name = "lblVersion"
+        lblVersion.Size = New Size(67, 25)
+        lblVersion.TabIndex = 1
+        lblVersion.Text = "Label1"
+        ' 
+        ' lblCopyright
+        ' 
+        lblCopyright.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblCopyright.Location = New Point(145, 93)
+        lblCopyright.Name = "lblCopyright"
+        lblCopyright.Size = New Size(263, 56)
+        lblCopyright.TabIndex = 1
+        lblCopyright.Text = "Label1"
+        ' 
+        ' lblDescription
+        ' 
+        lblDescription.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
+        lblDescription.Location = New Point(12, 164)
+        lblDescription.Name = "lblDescription"
+        lblDescription.Size = New Size(395, 189)
+        lblDescription.TabIndex = 1
+        lblDescription.Text = "Label1"
+        ' 
+        ' butCancel
+        ' 
+        butCancel.Location = New Point(330, 365)
+        butCancel.Name = "butCancel"
+        butCancel.Size = New Size(77, 35)
+        butCancel.TabIndex = 20
+        butCancel.Text = "E&xit"
+        butCancel.UseVisualStyleBackColor = True
+        ' 
+        ' About
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        CancelButton = butCancel
+        ClientSize = New Size(420, 404)
+        Controls.Add(butCancel)
+        Controls.Add(lblDescription)
+        Controls.Add(lblCopyright)
+        Controls.Add(lblVersion)
+        Controls.Add(lblProductname)
+        Controls.Add(PictureBox1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        KeyPreview = True
+        Name = "About"
+        ShowIcon = False
+        ShowInTaskbar = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "About"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
 
     End Sub
 
